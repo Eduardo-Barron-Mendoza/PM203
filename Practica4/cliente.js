8e106fa88563bddcd69077c86ba19259a8a69b07
Eduardo-Barron-Mendoza <123045976@upq.edu.mx>
2026-05-22 20:25:30 -0600

    Corrección cliente

--- Practica4/cliente.js
+++ Practica4/cliente.js
@@ -33,28 +33,20 @@ function productos_disponibles(productos){
 
 
 function esperar(tiempo){
-
     return new Promise(resolve => {
 
         setTimeout(resolve, tiempo);
 
     });
-
 }
 
 
 async function mostrar_estado_pedido(pedido){
 
-    for(const estado of pedido.pedidoEstatus){
-
-        console.log(`
-            Pedido #${pedido.id}
-            Producto: ${pedido.producto}
-            Estado: ${estado}
-        `);
-
-        await esperar(2000);
-
-    }
-
+    await esperar(2000);
+    console.log(`
+        Pedido #${pedido.id}
+        Producto: ${pedido.producto}
+        Estado: ${pedido.estatus}
+    `);
 }
