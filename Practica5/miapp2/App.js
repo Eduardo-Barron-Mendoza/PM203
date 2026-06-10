96c4411c53a217f6fc40fca94344d4be171bcf00
Eduardo-Barron-Mendoza <123045976@upq.edu.mx>
2026-06-09 20:16:12 -0600

    Practica6 parte 1

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -1,15 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+
+/* Zona1: Importaciones de componenetes y archivos */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
+
+
+/* Zona2: Main - Hogar de los componentes */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      
+    
+
+      <Text>------------ Componentes Nativos ------------</Text>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola mundo React Native</Text>
+      <Text>------------ Componentes Nativos ------------</Text>
+
+      <Text>------------ Componente Propio simple ------------</Text>
+      <Saludo></Saludo>
+
+      <Text>------------ Componente Propio simple ------------</Text>
+      <Saludo2></Saludo2>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+/* Zona3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Practica5/miapp2/components/Saludo2.js
@@ -0,0 +1,14 @@
+
+import {View, Button, Image, Text } from "react-native";
+
+
+export const Saludo2=()=>{
+    return (
+        <View>
+        <Image source={require('../assets/wave.png')}/>
+        <Text>Soy un componente compuesto</Text>
+        <Button title="Hola 203"> </Button>
+
+        </View>
+    )
+}
