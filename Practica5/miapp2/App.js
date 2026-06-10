dfcca155dccecd9e9dd9c297b250acb52f5646ce
Eduardo-Barron-Mendoza <123045976@upq.edu.mx>
2026-06-10 01:15:13 -0600

    Practica6 parte 2

--- Practica5/miapp2/App.js
+++ Practica5/miapp2/App.js
@@ -5,16 +5,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import {Saludo} from './components/Saludo';
 import {Saludo2} from './components/Saludo2';
+import {Perfil} from './components/Perfil';
 
 
 /* Zona2: Main - Hogar de los componentes */
 export default function App() {
   return (
     <View style={styles.container}>
-
-      
-    
-
+   
       <Text>------------ Componentes Nativos ------------</Text>
       <Image source={require('./assets/wave.png')}/>
       <Text>Hola mundo React Native</Text>
@@ -26,6 +24,8 @@ export default function App() {
       <Text>------------ Componente Propio simple ------------</Text>
       <Saludo2></Saludo2>
 
+      <Text>------------ Componente Propio Perfil ------------</Text>
+      <Perfil></Perfil>
       <StatusBar style="auto" />
 
     </View>
---
+++ Practica5/miapp2/components/Perfil.js
@@ -0,0 +1,16 @@
+
+import { View, Text } from 'react-native';
+
+export const Perfil=()=>{
+    return (
+        <View>
+
+        <Text>Nombre: Eduardo Barrón Mendoza</Text>
+        <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
+        <Text>Materia: Programación Móvil</Text>
+        <Text>Cuatrimestre: 9</Text>
+
+        </View>
+    )
+}
+
