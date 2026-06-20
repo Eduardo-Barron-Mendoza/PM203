1d7e7cb48c558c14fc125063f073336a55caa11c
Eduardo-Barron-Mendoza <123045976@upq.edu.mx>
2026-06-19 20:18:45 -0600

    Pressable & Switch

--- Practica5/miapp2/screens/PressableScreen.js
+++ Practica5/miapp2/screens/PressableScreen.js
@@ -2,16 +2,50 @@
 
 /* Zona1: Importaciones de componenetes y archivos */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Switch, Pressable } from 'react-native';
 
 
 /* Zona2: Main - Hogar de los componentes */
 export default function PressableScreen() {
+
+  const[isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleSwitch = () => setIsDarkMode(previousState => !previousState)
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, isDarkMode ? styles.bgDark : styles.bgLight]}>
 
-        <Text>Aqui va la practica de Roque</Text>
+      <Text style={[styles.title, isDarkMode ? styles.textDark : styles.textLight]}> Ajustes del texto </Text>
 
+      <View style={styles.row}>
+        <Text style={[styles.label, isDarkMode ? styles.textDark : styles.textLight]}> 
+          Modo Oscuro {isDarkMode ? 'Activado' : 'Desactivado'}
+        </Text>  
+        <Switch
+        trackColor = {{false:'black', true:'white'}}
+        thumbColor = {isDarkMode ? 'yellow' : 'gray'} 
+        onValueChange={toggleSwitch}
+        value={isDarkMode}
+        disabled={false}
+        />
+
+      </View>
+
+      <Pressable 
+      onPress={()=> Alert.alert('Exito','Configuracion guardada')}
+      onLongPress={() => Alert.alert('Informacion', 'Se mantuvo presionado')}
+      delayLongPress={1000}
+      style={({pressed}) => [
+        styles.button,
+        {backgroundColor: pressed ? 'blue' : 'green'}
+      ]}>
+
+      <Text style={styles.buttonText}> Guardar cambios</Text>
+
+      </Pressable>
+
+      
       <StatusBar style="auto" />
 
     </View>
@@ -22,10 +56,37 @@ export default function PressableScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection:'column'
+    padding: 20,
   },
-
+  bgLight: { backgroundColor: '#f5f5f5' },
+  bgDark: { backgroundColor: '#121212' },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  textLight: { color: '#000' },
+  textDark: { color: '#fff' },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 40,
+    paddingHorizontal: 20,
+  },
+  label: { fontSize: 18 },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    elevation: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
